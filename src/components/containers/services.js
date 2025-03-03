95340b410ee02e928cbd4ca53fe9697afee4c2dd
Web4 <167559384+Web4application@users.noreply.github.com>
2025-03-03 15:12:54 -0500

    Update and rename src/components/containers/services.js to src/components/containers/services/Api.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- src/components/containers/services.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'https://api.example.com/items';
-
-export const fetchItems = async () => {
-    const response = await axios.get(API_URL);
-    return response.data;
-};
-
