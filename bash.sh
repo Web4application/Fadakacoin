d4979bee05e292041966d698f0209b49fc86e08e
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-09 21:59:33 +0200

    Update bash.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- bash.sh
+++ bash.sh
@@ -1,2 +1,8 @@
 npm install --save-dev hardhat
 npx hardhat compile
+git clone https://github.com/Web4application/fadaka-blockchain.git
+cd fadaka-blockchain
+cd backend
+npm install
+cd frontend
+npm install
