0f7822178ab5b0011812fef9515e311ceec07d2c
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-13 02:13:26 +0200

    Update bash.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- @Fadaka-blockchain/bash.sh
+++ @Fadaka-blockchain/bash.sh
@@ -1,2 +1,6 @@
-git clone https://github.com/Web4application/fadaka-blockchain.git
-cd fadaka-blockchain
+$ git clone https://github.com/Web4application/fadaka-blockchain.git
+$ cd fadaka-blockchain
+
+$ git clone https://github.com/kaspanet/kaspad
+$ cd kaspad
+$ go install . ./cmd/...
