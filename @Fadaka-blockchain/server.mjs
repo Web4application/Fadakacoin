6fdc0066294db03a1cc0715f682a9f736bf97783
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-13 02:01:50 +0200

    Update and rename server.mjs to server.mjs
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- @Fadaka-blockchain/server.mjs
+++ @Fadaka-blockchain/server.mjs
@@ -8,7 +8,7 @@ const server = createServer((req, res) => {
 
 // starts a simple http server locally on port 3000
 server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+  console.log('Listening on 102.89.40.15:8000');
 });
 
 // run with `node server.mjs`
