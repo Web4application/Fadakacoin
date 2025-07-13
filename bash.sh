ed2ff6c63772a2ea89b985a03240eb62f18e6468
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-13 02:23:46 +0200

    Update and rename Bash.sh to bash.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

---
+++ bash.sh
@@ -0,0 +1,19 @@
+$ npm install --save-dev hardhat
+
+$ npx hardhat compile
+
+$ git clone https://github.com/Web4application/fadaka-blockchain.git
+cd fadaka-blockchain
+
+cd backend
+$ npm install
+
+cd frontend
+$ npm install
+
+pip install fastapi uvicorn
+uvicorn backend:app --reload
+
+$ npm install
+$ npm run dev
+$ npm create vite@latest vanilla-ts-6963 -- --template vanilla-ts
