82bdb3f9d035fe8ba1bedf69acd242254d119711
Seriki yakub <sandraisom00@gmail.com>
2025-12-27 19:14:48 +0100

    Create mnemonic.ts
    Signed-off-by: Seriki yakub <sandraisom00@gmail.com>

---
+++ packages/wallet/src/mnemonic.ts
@@ -0,0 +1,10 @@
+import { generateMnemonic as generate, validateMnemonic as validate } from "@scure/bip39";
+import { wordlist as english } from "@scure/bip39/wordlists/english.js";
+
+export function generateMnemonic(strength = 160, wordlist = english): string {
+  return generate(wordlist, strength);
+}
+
+export function validateMnemonic(mnemonic: string, wordlist = english): boolean {
+  return validate(mnemonic, wordlist);
+}
